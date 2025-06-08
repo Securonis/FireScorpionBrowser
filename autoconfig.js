a3f9fffefac67409fbb39bfcc4f2e6dbeb2f741a
Emir <142451159+root0emir@users.noreply.github.com>
2025-06-08 22:11:50 +0300

    Delete autoconfig.js

--- autoconfig.js
+++
@@ -1,8 +0,0 @@
-// IMPORTANT: Start your code on the 2nd line
-// First line is a comment for Firefox to process this file
-
-// Enable automatic configuration - Securonis hardened configuration
-pref("general.config.filename", "securonis.cfg");
-pref("general.config.obscure_value", 0);
-pref("general.config.sandbox_enabled", false);
-
