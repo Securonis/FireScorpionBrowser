9ce7781a96a20679ec2f0b96301298d8dff4c38b
Emir <142451159+root0emir@users.noreply.github.com>
2025-06-08 18:35:54 +0300

    Update autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,1 +1,8 @@
+// IMPORTANT: Start your code on the 2nd line
+// First line is a comment for Firefox to process this file
+
+// Enable automatic configuration - Securonis hardened configuration
+pref("general.config.filename", "securonis.cfg");
+pref("general.config.obscure_value", 0);
+pref("general.config.sandbox_enabled", false);
 
